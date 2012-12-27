98066953975e06b321f78d4f3bed015a0d9fd1a5
ilumos <ilumos@gmail.com>
2012-12-27 17:40:07 +0000

    fixed logged in user profile link

--- application/views/partials/logged_in.blade.php
+++ application/views/partials/logged_in.blade.php
@@ -1,7 +1,7 @@
 <li class="dropdown pull-right">
 	<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{$logged_in_user_avatar_small}}"> {{$logged_in_user}} <b class="caret"></b></a>
 	<ul class="dropdown-menu">
-		<li><a href="{{URL::to_action('user@profile')}}">Edit Profile</a></li>
+		<li><a href="{{URL::to_action('user@profile',array(Session::get('user_id')))}}">Profile</a></li>
 		<li><a href="{{URL::to_action('user@preferences')}}">Preferences</a></li>
 		<li><a href="{{URL::to_action('user@logout')}}">Log Out</a></li>
 	</ul>
