06d5a791da2cdf9011088567ffac4e285b971766
ilumos <ilumos@gmail.com>
2013-01-20 17:39:26 +0000

    added license information to footer

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -91,7 +91,7 @@
 			<hr>
 
 			<footer>
-				<p>Footer</p>
+				<p><a href="https://github.com/ilumos/lanager" target="_blank">LANager</a> is copyright &copy; {{date('Y')}} Oliver Baker and licensed under <a href="http://www.gnu.org/licenses/agpl-3.0.html" target="_blank">AGPLv3</a></p>
 			</footer>
 		</div> <!-- /container -->
 	</body>
