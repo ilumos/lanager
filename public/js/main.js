9728ab827383320d584fb198cf207e20dc027a0c
ilumos <ilumos@gmail.com>
2013-03-24 17:28:42 +0000

    changed playback system console messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 function onYouTubePlayerReady(playerId)
 {
 	yt_player = document.getElementById('player_id');
-	console.log('Playlist: YouTube video player ready');
+	console.log('Playlist: Embedded video player ready');
 	yt_player.setPlaybackQuality('highres');
 	yt_player.addEventListener('onStateChange', 'onStateChangeHandler');
 	yt_player.playVideo();
@@ -13,20 +13,19 @@ var currentPlaybackState;
 
 function pollPlaylist(firstCall)
 {
-	console.log('Playlist: Polling for changes');
 	var url = 'http://localhost/playlist/get_entry';
 	
 	// Getting the first video ID for the embed code
 	if(firstCall)
 	{
+		console.log('Playlist: Retreiving first entry');
 		$.ajax({
 			url: url,
 			dataType: 'json',
 			async: false,
 	  		success: function(entry)
 	  		{
-				console.log('Playlist: Polling success');
-				console.log('Playlist: Setting nowPlayingId for the first time');
+				console.log('Playlist: Entry '+entry.id+' set as first video');
 				nowPlayingId = entry.id;
 			},
 			error: function(response)
@@ -39,10 +38,11 @@ function pollPlaylist(firstCall)
 	{
 		$.getJSON(url,function(entry)
 		{
+			console.log('Playlist: Polling for changes');
 			// new entry, or entry skipped/deleted
 			if(entry.id != nowPlayingId)
 			{
-				console.log('Playlist: Changing video to: '+entry.id);
+				console.log('Playlist: Changing to entry: '+entry.id);
 				loadAndPlayEntry(entry.id);
 			}
 			if(entry.playback_state != currentPlaybackState)
@@ -50,12 +50,12 @@ function pollPlaylist(firstCall)
 				switch(entry.playback_state)
 				{
 					case 1: // playing
-						console.log('Playlist: Setting state to "playing"');
+						console.log('Playlist: Entry '+entry.id+' setting state to "playing"');
 						yt_player.playVideo();
 						currentPlaybackState = 1;
 						break;
 					case 2: // paused
-						console.log('Playlist: Setting state to "paused"');
+						console.log('Playlist: Entry '+entry.id+' setting state to "paused"');
 						yt_player.pauseVideo();
 						currentPlaybackState = 2;
 						break;
@@ -71,7 +71,7 @@ function loadAndPlayEntry(videoId)
 {
 	if(videoId)
 	{
-		console.log('Playlist: Loading and playing video: '+videoId);
+		console.log('Playlist: Entry '+videoId+' loading and playing');
 		yt_player.cueVideoById(videoId);
 		yt_player.setPlaybackQuality('highres');
 		yt_player.playVideo();
@@ -86,13 +86,13 @@ function loadAndPlayEntry(videoId)
 	}
 }
 
-function markEntry(videoId, playbackState)
+function markEntry(videoId, playbackState, playbackStateLabel)
 {
 	if(videoId)
 	{
-		console.log('Playlist: Marking entry: '+videoId+' as '+playbackState);
+		console.log('Playlist: Entry '+videoId+' marking as '+playbackStateLabel+' ('+playbackState+')');
 		$.get('http://localhost/playlist/mark_entry/'+videoId+'/'+playbackState, function(response) {
-			console.log('Playlist: Marking response: '+response+' rows affected');
+			console.log('Playlist: Entry '+videoId+' marking response: '+response+' rows affected ('+playbackStateLabel+')');
 		});
 	}
 }
@@ -101,25 +101,27 @@ function onStateChangeHandler(newState) {
 	switch(newState)
 	{
 		case -1:
-			console.log('Playlist: Video unstarted ('+newState+')');
+			console.log('Playlist: Entry '+nowPlayingId+' is unstarted ('+newState+')');
 			break;
 		case 0:
-			console.log('Playlist: Video ended ('+newState+')');
-			markEntry(nowPlayingId, 4); // mark the last video as played
+			console.log('Playlist: Entry '+nowPlayingId+' has ended ('+newState+')');
+			markEntry(nowPlayingId, 4, 'played'); // mark the last video as played
 			break;
 		case 1:
-			console.log('Playlist: Video playing ('+newState+')');
-			markEntry(nowPlayingId, 1); // mark the last video as playing
+			console.log('Playlist: Entry '+nowPlayingId+' is now playing ('+newState+')');
+			markEntry(nowPlayingId, 1, 'playing'); // mark the last video as playing
+			currentPlaybackState = 1;
 			break;
 		case 2:
-			console.log('Playlist: Video paused ('+newState+')');
-			markEntry(nowPlayingId, 2); // mark the last video as paused
+			console.log('Playlist: Entry '+nowPlayingId+' is now paused ('+newState+')');
+			markEntry(nowPlayingId, 2, 'paused'); // mark the last video as paused
+			currentPlaybackState = 2;
 			break;
 		case 3:
-			console.log('Playlist: Video buffering ('+newState+')');
+			console.log('Playlist: Entry '+nowPlayingId+' is now buffering ('+newState+')');
 			break;
 		case 5:
-			console.log('Playlist: Video cued ('+newState+')');
+			console.log('Playlist: Entry '+nowPlayingId+' has been cued ('+newState+')');
 			break;
 	}
 }
