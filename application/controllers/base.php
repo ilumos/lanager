91d6965c7fd4a3e2b1dd3aafe48a37a1159ba5d0
ilumos <ilumos@gmail.com>
2013-06-16 21:01:03 +0100

    adding start/end time picker to event form

--- application/controllers/base.php
+++ application/controllers/base.php
@@ -20,10 +20,12 @@ class Base_Controller extends Controller {
 		Asset::add('jquery', 'js/jquery-1.8.2.min.js');
 		Asset::add('modernizr', 'js/vendor/modernizr-2.6.2-respond-1.1.0.min.js');
 		Asset::add('bootstrap-js', 'js/vendor/bootstrap.min.js');
+		Asset::add('bootstrap-datetimepicker', 'js/vendor/bootstrap-datetimepicker.min.js');
 		Asset::add('main', 'js/main.js');
 		Asset::add('swfobject', 'js/swfobject.js');
 
 		Asset::add('bootstrap', 'css/bootstrap.css');
+		Asset::add('bootstrap-datetimepicker', 'css/bootstrap-datetimepicker.min.css');
 		Asset::add('bootstrap-responsive', 'css/bootstrap-responsive.min.css');
 		Asset::add('style', 'css/main.css');
 		parent::__construct();
--- application/views/event/form.blade.php
+++ application/views/event/form.blade.php
@@ -1,7 +1,16 @@
 @layout('layouts/default')
 @section('content')
 
-<?php array_unshift($event_types, NULL); array_unshift($managers, NULL); ?>
+<?php
+// prepare form data
+
+// add empty dropdown items to optional fields
+array_unshift($event_types, NULL);
+array_unshift($managers, NULL);
+
+$hours = range(0, 24);
+$minutes = array('00','30');
+?>
 
 <h3>New Event</h3>
 
@@ -10,9 +19,40 @@
 {{ Form::label('name', 'Name') }}
 {{ Form::text('name',NULL,array('placeholder' => 'e.g. Team Fortress 2', 'maxlength' => 200)) }}
 <br>
-{{ Form::label('details', 'Details') }}
+{{ Form::label('description', 'Description') }}
 {{ Form::textarea('details',NULL,array('placeholder' => 'Add more info', 'rows' => 4)) }}
 <br>
+{{ Form::label('start', 'Starts') }}
+<div id="start_datetimepicker" class="input-append date">
+    <input id="start" data-format="dd/MM/yyyy hh:mm:ss" type="text"></input>
+    <span class="add-on">
+      <i data-time-icon="icon-time" data-date-icon="icon-calendar">
+      </i>
+    </span>
+</div>
+{{ Form::label('end', 'Ends') }}
+<div id="end_datetimepicker" class="input-append date">
+    <input id="end" data-format="dd/MM/yyyy hh:mm:ss" type="text"></input>
+    <span class="add-on">
+      <i data-time-icon="icon-time" data-date-icon="icon-calendar">
+      </i>
+    </span>
+</div>
+
+
+<script type="text/javascript">
+  $(function() {
+    $('#start_datetimepicker').datetimepicker({
+      language: 'en'
+    });
+    $('#end_datetimepicker').datetimepicker({
+      language: 'en'
+    });
+  });
+</script>
+
+
+
 {{ Form::label('type', 'Type') }}
 {{ Form::select('type', $event_types) }}
 <br>
