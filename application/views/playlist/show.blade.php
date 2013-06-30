40302fed37c3dd498a39919132325fc880bb672e
ilumos <ilumos@gmail.com>
2013-06-30 18:39:52 +0100

    removed redundant playlist controls

--- application/views/playlist/show.blade.php
+++ application/views/playlist/show.blade.php
@@ -3,7 +3,7 @@
 <h3>Playlist</h3>
 @include('playlist.form')<br>
 @if(Config::get('lanager.playlist_screen_allowed_user') == Session::get('user_id'))
-	@include('playlist.controls')
+	<a href="{{URL::to_action('playlist@screen')}}" target="_blank">Open Screen</a><br><br>
 @endif
 @include('playlist.list')
 <br>
