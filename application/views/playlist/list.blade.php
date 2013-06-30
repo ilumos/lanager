79e2f3abfa5682cf3fd216667f2b8cdba22cf9e2
ilumos <ilumos@gmail.com>
2013-06-30 17:56:36 +0100

    added duration display to playlist page

--- application/views/playlist/list.blade.php
+++ application/views/playlist/list.blade.php
@@ -1,17 +1,27 @@
-@forelse ($playlist_entries->results as $entry)
-	<a href="{{URL::to_action('user@profile',$entry->user->id)}}"><img src="{{$entry->user->avatar_small}}" title="{{ e($entry->user->username) }}"></a>
-	&nbsp;
-	@if($entry->playback_state == 3)
-		<em>(Skipped)</em>
-	@endif
-	{{ e($entry->title) }}
-	@if(Config::get('lanager.playlist_screen_allowed_user') == Session::get('user_id'))
-		<a href="{{URL::to_action('playlist@delete_entry',$entry->id)}}">X</a>
-	@endif
-	<br>
-	
-@empty
-	<p>No entries to show!</p>
-@endforelse
+<table id="playlist">
+	<tbody>
+	@forelse ($playlist_entries->results as $entry)
+		<tr class="playlist_entry{{ ($entry->playback_state == 1) ? ' entry_now_playing' : ''}}">
+			<td class="playlist_entry_user">
+				<a href="{{URL::to_action('user@profile',$entry->user->id)}}"><img src="{{$entry->user->avatar_small}}" title="{{ e($entry->user->username) }}"></a>
+			</td>
+			<td class="playlist_entry_duration">
+				{{ e(intval(gmdate('i',$entry->duration)).':'.gmdate('s',$entry->duration)) }}
+			</td>
+			<td class="playlist_entry_title">
+				{{ e($entry->title) }}
+			</td>
+			<td class="playlist_entry_controls">
+				@if(Config::get('lanager.playlist_screen_allowed_user') == Session::get('user_id'))
+					<a href="{{URL::to_action('playlist@delete_entry',$entry->id)}}">X</a>
+				@endif
+			</td>
+		</tr>	
+	@empty
+		<tr colspan="3">No entries to show!</tr>
+	@endforelse
+	</tbody>
+</table>
+
 
 {{$playlist_entries->links()}}
