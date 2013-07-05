dd6b434f15edb3e00d0d5c81016d2f753af684f1
ilumos <ilumos@gmail.com>
2013-07-05 18:51:53 +0100

    added collection of user's ip on login

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -32,6 +32,7 @@ class User_Controller extends Base_Controller {
 			// Add or update their details
 			$user->steam_id_64 = $steamId64;
 			$user->username = $SteamProfile->getProfileName();
+			$user->ip = getenv("REMOTE_ADDR");
 			$user->avatar_small = $SteamProfile->getAvatarSmall();
 			$user->avatar_medium = $SteamProfile->getAvatarMedium();
 			$user->avatar_large = $SteamProfile->getAvatarLarge();
