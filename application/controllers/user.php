9f66c923ec7f7bf2e6ce3facba303341abd8303b
ilumos <ilumos@gmail.com>
2013-07-06 00:15:45 +0100

    added 404 for unknown user profiles & tidied up comments

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -7,7 +7,8 @@ class User_Controller extends Base_Controller {
 		//
 	}
 
-	public function action_login()
+
+	public function action_login() // log a steam user in
 	{
 		$LightOpenId = new LightOpenId;
 		if(Input::has('openid_mode')) {
@@ -22,10 +23,10 @@ class User_Controller extends Base_Controller {
 			$SteamProfile = new SteamProfile($steamId64);
 			$SteamProfile->fetchProfile();
 
-			// See if the user has logged in previously
+			// See if the user is already in the database
 			$user = LANager\User::where('steam_id_64', '=', $steamId64)->first();
 	
-			// If they have not, create them in the database as an attendee
+			// If they have not, create them in the database and assign the attendee role
 			if($user == NULL) {
 				$user = new LANager\User;
 				$roles[] = LANager\Role::where('name','=','attendee')->first();
@@ -33,18 +34,19 @@ class User_Controller extends Base_Controller {
 				// If they are the first user to sign in (or the first user after the initial seed)
 				if(LANager\User::count() <= 1)
 				{
-					// Give them admin privileges
+					// Also give them admin privileges
 				$roles[] = LANager\Role::where('name','=','admin')->first();
 				}
 			}
 
-			// Add or update their details
+			// Add (or update) their details in the database
 			$user->steam_id_64 = $steamId64;
 			$user->username = $SteamProfile->getProfileName();
 			$user->ip = getenv("REMOTE_ADDR");
 			$user->avatar = $SteamProfile->getAvatarSmall();
 			$user->save();
 
+			// Assign the required roles
 			if(isset($roles))
 			{
 				foreach($roles as $role)
@@ -59,32 +61,41 @@ class User_Controller extends Base_Controller {
 		}
 		return Redirect::home();
 	}
-	
+
+
 	public function action_logout()
 	{
 		Auth::logout();
 		return Redirect::back();
 	}
 
-	public function action_profile($user_id)
+
+	public function action_profile($user_id) // Display a given user's profile
 	{
-		$user = LANager\User::find($user_id);
-		$steamProfile = new SteamProfile($user->steam_id_64);
-		$steamProfile->fetchProfile();
-		return View::make('user.profile')
-					->with('title',$user->username)
-					->with('user',$user)
-					->with('steamProfile',$steamProfile)
-					->with('shouts',$user->shouts()->order_by('id', 'desc')->paginate(10)); // recent shouts
+		// Retrieve the user (if they exist)
+		if($user = LANager\User::find($user_id))
+		{
+			$steamProfile = new SteamProfile($user->steam_id_64);
+			$steamProfile->fetchProfile();
+			return View::make('user.profile')
+						->with('title',$user->username)
+						->with('user',$user)
+						->with('steamProfile',$steamProfile)
+						->with('shouts',$user->shouts()->order_by('id', 'desc')->paginate(10)); // recent shouts
+		}
+		else
+		{
+			return Response::error('404');
+		}
 	}
 
-	public function action_list()
+
+	public function action_list() // Show a paginated user list
 	{
 		$users = LANager\User::order_by('username', 'asc')->paginate(10);
 		return View::make('user.list')
 					->with('title','People')
 					->with('users',$users);
-
 	}
 
 
